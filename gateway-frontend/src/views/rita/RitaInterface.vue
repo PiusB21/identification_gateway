cf7d59f4444a735500e9f3a5c71f6be62f76dac4
CodeIsLife <pbsemufali21@gmail.com>
2025-05-13 10:59:57 +0300

    13/5

--- gateway-frontend/src/views/rita/RitaInterface.vue
+++ gateway-frontend/src/views/rita/RitaInterface.vue
@@ -4,7 +4,7 @@
       <div class="w-[90%] mx-auto text-3xl flex justify-between">
         <div>RITA</div>
         <v-btn
-          @click="router.push('rita')"
+          @click="logout()"
           color="white"
           icon="mdi-logout"
           title="Logout"
@@ -22,6 +22,7 @@
 import { ref } from 'vue'
 import CitizenData from '../admin/CitizenData.vue'
 import { useRouter } from 'vue-router'
+import {logout} from "@/utils/contractService.js"
 
 const router = useRouter()
 
